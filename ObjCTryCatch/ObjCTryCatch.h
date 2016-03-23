48777c359e79d9513679bfc028fc1d45f7eefa6e
Jon Brooks <thomas.brooks@mheducation.com>
2016-03-23 13:36:29 -0700

    Initial Commit

---
+++ ObjCTryCatch/ObjCTryCatch.h
@@ -0,0 +1,19 @@
+//
+//  ObjCTryCatch.h
+//  ObjCTryCatch
+//
+//  Created by jbrooks on 3/23/16.
+//  Copyright © 2016 jbrooks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ObjCTryCatch.
+FOUNDATION_EXPORT double ObjCTryCatchVersionNumber;
+
+//! Project version string for ObjCTryCatch.
+FOUNDATION_EXPORT const unsigned char ObjCTryCatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjCTryCatch/PublicHeader.h>
+
+
